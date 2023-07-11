53e776e0821c345a9ca820c51d3fa3b95b337baa
dwu0042 <david.wu1@monash.edu>
2023-07-11 13:15:43 +1000

    Correct discretisation and edge creation

--- make_temporal_network.py
+++ make_temporal_network.py
@@ -1,13 +1,12 @@
 from temporal_network import TemporalNetwork
 import polars as pl
-import numpy as np
 import datetime
 
 _REF_DATE = datetime.datetime(year=2017, month=3, day=1)
 
 def normalise_dates(df: pl.DataFrame, cols, ref_date=_REF_DATE):
     return df.with_columns(*(
-        (pl.col(col) - _REF_DATE).dt.seconds() /60/60/24
+        (pl.col(col) - ref_date).dt.seconds() /60/60/24
         for col in cols
     )
     )
@@ -18,14 +17,20 @@ def convert_presence_to_network(presence: pl.DataFrame, discretisation=1, return
     G = TemporalNetwork()
 
     presence = (presence.sort(pl.col('Adate'))
-                        .with_columns(pl.arange(pl.col('Adate'), pl.col('Ddate')+1).alias('present'))
+                        .with_columns(
+                            pl.arange(
+                                pl.col('Adate').floordiv(discretisation)*discretisation, 
+                                pl.col('Ddate')+1, 
+                                discretisation)
+                            .alias('present'))
                         .explode('present')
     )
 
     for partition in presence.partition_by('sID'):
-        for row0, row1 in zip(partition.iter_rows(), partition.shift(1).iter_rows()):
-            if row1[-1] is not None and row1[-1] - row0[-1] < return_window:
-                nodes = ((row0[1], row0[-1]), (row1[1], row1[-1]))
+        partition = partition.sort('present', 'Adate').select('fID', 'present')
+        for row0, row1 in zip(partition.shift(1).iter_rows(), partition.iter_rows()):
+            if row0[-1] is not None and ((row1[-1] - row0[-1]) < return_window):
+                nodes = (row0, row1)
                 w = G.edges.get(nodes, _empty_edge)['weight'] + 1
                 G.add_edge(*nodes, weight=w)
 
